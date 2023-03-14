2a0a68a7ccbad8664930dcf7de30b5e465e29ec2
Yankevych Pavlo <pawabiker@gmail.com>
2023-03-14 10:06:56 +0200

    First test commit

---
+++ admin/config.php
@@ -0,0 +1,37 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://oc.summon-rain.com/admin/');
+define('HTTP_CATALOG', 'http://oc.summon-rain.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://oc.summon-rain.com/admin/');
+define('HTTPS_CATALOG', 'https://oc.summon-rain.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/summonra/oc.summon-rain.com/admin/');
+define('DIR_SYSTEM', '/home/summonra/oc.summon-rain.com/system/');
+define('DIR_IMAGE', '/home/summonra/oc.summon-rain.com/image/');
+define('DIR_STORAGE', '/home/summonra/oc_bt_loc_storage/');
+define('DIR_CATALOG', '/home/summonra/oc.summon-rain.com/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'summonra_oc_store_user');
+define('DB_PASSWORD', 'difficultpassword322');
+define('DB_DATABASE', 'summonra_oc_store');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
+define('OPENCARTFORUM_SERVER', 'https://opencartforum.com/');
